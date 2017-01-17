d054dda58d707362ab92871c5397871d84b3c852
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-01-17 16:00:29 +0300

    Add Qualifiers for local and remote repositories Add MessageTypes constants class

--- app/src/main/java/com/nomensvyat/offlinechat/model/services/message/MessageService.java
+++ app/src/main/java/com/nomensvyat/offlinechat/model/services/message/MessageService.java
@@ -1,5 +1,7 @@
 package com.nomensvyat.offlinechat.model.services.message;
 
+import com.nomensvyat.offlinechat.di.Local;
+import com.nomensvyat.offlinechat.di.Remote;
 import com.nomensvyat.offlinechat.di.application.PerApplication;
 import com.nomensvyat.offlinechat.model.entities.network.message.RawMessage;
 import com.nomensvyat.offlinechat.model.repositories.message.MessageRepository;
@@ -7,25 +9,38 @@ import com.nomensvyat.offlinechat.model.repositories.message.MessageRepository;
 import javax.inject.Inject;
 
 import rx.Observable;
+import rx.Single;
 import rx.subjects.PublishSubject;
 
 @PerApplication
 public class MessageService {
 
-    private final MessageRepository messageRepository;
+    private final MessageRepository localMessageRepository;
+    private final MessageRepository remoteMessageRepository;
     private PublishSubject<RawMessage> newMessageSubject = PublishSubject.create();
 
     @Inject
-    public MessageService(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
+    public MessageService(@Local MessageRepository localMessageRepository,
+            @Remote MessageRepository remoteMessageRepository) {
+        this.localMessageRepository = localMessageRepository;
+        this.remoteMessageRepository = remoteMessageRepository;
     }
 
     public void onNewMessage(RawMessage rawMessage) {
-        messageRepository.saveMessage(rawMessage)
+        localMessageRepository.saveMessage(rawMessage)
                 .subscribe(newMessageSubject::onNext);
     }
 
     public Observable<RawMessage> observeNewMessages() {
         return newMessageSubject;
+    }
+
+    public Single<RawMessage> sendMessage(RawMessage rawMessage) {
+        //save unsend message
+        return localMessageRepository.saveMessage(rawMessage)
+                //send message
+                .flatMap(remoteMessageRepository::saveMessage)
+                //resave message with server generated id
+                .flatMap(localMessageRepository::saveMessage);
     }
 }
--- app/src/test/java/com/nomensvyat/offlinechat/model/services/message/MessageServiceTest.java
+++ app/src/test/java/com/nomensvyat/offlinechat/model/services/message/MessageServiceTest.java
@@ -1,9 +1,12 @@
 package com.nomensvyat.offlinechat.model.services.message;
 
+import com.nomensvyat.offlinechat.model.entities.network.message.MessageTypes;
 import com.nomensvyat.offlinechat.model.entities.network.message.RawMessage;
 import com.nomensvyat.offlinechat.model.repositories.message.FakeMessageRepository;
 
 import org.junit.Test;
+
+import java.util.Calendar;
 
 import rx.observers.AssertableSubscriber;
 
@@ -11,9 +14,16 @@ public class MessageServiceTest {
 
     @Test
     public void onNewMessage() {
-        MessageService messageService = new MessageService(new FakeMessageRepository());
+        MessageService messageService = new MessageService(new FakeMessageRepository(),
+                                                           new FakeMessageRepository());
 
-        RawMessage rawMessage = new RawMessage();
+        RawMessage rawMessage = RawMessage.builder()
+                .id(1)
+                .message("some message")
+                .datetime(Calendar.getInstance().getTimeInMillis())
+                .roomId(1)
+                .type(MessageTypes.IN)
+                .build();
 
         AssertableSubscriber<RawMessage> test = messageService.observeNewMessages()
                 .test();
