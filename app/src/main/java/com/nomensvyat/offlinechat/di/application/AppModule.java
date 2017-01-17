a06507c330d01edfaa7e8b5e6cd30ad6e1b8333d
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-01-17 19:50:27 +0300

    Implement LocalMessageRepository

--- app/src/main/java/com/nomensvyat/offlinechat/di/application/AppModule.java
+++ app/src/main/java/com/nomensvyat/offlinechat/di/application/AppModule.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.nomensvyat.offlinechat.di.Local;
 import com.nomensvyat.offlinechat.di.Remote;
+import com.nomensvyat.offlinechat.model.entities.persistent.PersistentMessageDao;
 import com.nomensvyat.offlinechat.model.repositories.message.FakeRemoteRepository;
 import com.nomensvyat.offlinechat.model.repositories.message.LocalMessageRepository;
 import com.nomensvyat.offlinechat.model.repositories.message.MessageRepository;
@@ -29,7 +30,7 @@ public class AppModule {
     @Provides
     @PerApplication
     @Local
-    MessageRepository provideLocalMessageRepository() {
-        return new LocalMessageRepository();
+    MessageRepository provideLocalMessageRepository(PersistentMessageDao persistentMessageDao) {
+        return new LocalMessageRepository(persistentMessageDao);
     }
 }
--- app/src/main/java/com/nomensvyat/offlinechat/model/repositories/message/LocalMessageRepository.java
+++ app/src/main/java/com/nomensvyat/offlinechat/model/repositories/message/LocalMessageRepository.java
@@ -1,25 +1,60 @@
 package com.nomensvyat.offlinechat.model.repositories.message;
 
+import com.nomensvyat.offlinechat.model.entities.Message;
+import com.nomensvyat.offlinechat.model.entities.mapper.Mapper;
+import com.nomensvyat.offlinechat.model.entities.mapper.MessageMappers;
 import com.nomensvyat.offlinechat.model.entities.network.message.RawMessage;
+import com.nomensvyat.offlinechat.model.entities.persistent.PersistentMessage;
+import com.nomensvyat.offlinechat.model.entities.persistent.PersistentMessageDao;
 
 import java.util.List;
 
+import rx.Observable;
 import rx.Single;
+import rx.schedulers.Schedulers;
 
 public class LocalMessageRepository implements MessageRepository {
 
+    private PersistentMessageDao persistentMessageDao;
+
+    public LocalMessageRepository(PersistentMessageDao persistentMessageDao) {this.persistentMessageDao = persistentMessageDao;}
+
     @Override
     public Single<List<RawMessage>> getMessages() {
-        return null;
+        return getAllMessages()
+                .observeOn(Schedulers.computation())
+                .flatMap(Observable::from)
+                .map(MessageMappers.createToRawMessageMapper()::map)
+                .toList()
+                .first()
+                .toSingle();
+    }
+
+    private Observable<List<PersistentMessage>> getAllMessages() {
+        return persistentMessageDao.rx()
+                .loadAll();
     }
 
     @Override
     public Single<List<RawMessage>> getMessages(long roomId) {
-        return null;
+        return getAllMessages()
+                .observeOn(Schedulers.computation())
+                .flatMap(Observable::from)
+                .filter(persistentMessage -> persistentMessage.getRoomId() == roomId)
+                .map(MessageMappers.createToRawMessageMapper()::map)
+                .toList()
+                .first()
+                .toSingle();
     }
 
     @Override
     public Single<RawMessage> saveMessage(RawMessage rawMessage) {
-        return null;
+        Mapper<Message, PersistentMessage> toPersistentMessageMapper = MessageMappers.createToPersistentMessageMapper();
+        PersistentMessage persistentMessage = toPersistentMessageMapper.map(rawMessage);
+        return persistentMessageDao.rx()
+                .save(persistentMessage)
+                .first()
+                .map(MessageMappers.createToRawMessageMapper()::map)
+                .toSingle();
     }
 }
