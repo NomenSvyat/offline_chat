8370dd2c0be9f62ab19bd1d109965823b309ef8e
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-01-26 17:27:09 +0300

    Start chat for Room 1 on app startup

--- app/src/main/java/com/nomensvyat/offlinechat/MainActivity.java
+++ app/src/main/java/com/nomensvyat/offlinechat/MainActivity.java
@@ -1,13 +1,20 @@
 package com.nomensvyat.offlinechat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nomensvyat.offlinechat.model.entities.Room;
+import com.nomensvyat.offlinechat.ui.chat_messaging.ChatMessagingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startActivity(
+                ChatMessagingActivity.createStartIntent(this, Room.createRoom(1)));
+
+        finish();
     }
 }
