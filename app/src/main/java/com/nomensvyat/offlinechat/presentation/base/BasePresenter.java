af8f7ecc54691fef173f4116d364fdfdcdd6b2e1
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-01-28 15:04:58 +0300

    Add LifeCycleMvpPresenter support to BaseActivity

--- app/src/main/java/com/nomensvyat/offlinechat/presentation/base/BasePresenter.java
+++ app/src/main/java/com/nomensvyat/offlinechat/presentation/base/BasePresenter.java
@@ -2,11 +2,40 @@ package com.nomensvyat.offlinechat.presentation.base;
 
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
 import com.hannesdorfmann.mosby.mvp.MvpView;
+import com.nomensvyat.offlinechat.utils.rx.LifecycleCompositeSubscription;
+import com.nomensvyat.offlinechat.utils.rx.LifecycleCompositeSubscriptionFabric;
+
+import rx.Subscription;
 
 /**
  * Can be used later for adding something like lifecycle methods.
  * Now it's no-op
  */
-public abstract class BasePresenter<V extends MvpView> extends MvpBasePresenter<V> {
+public abstract class BasePresenter<V extends MvpView>
+        extends MvpBasePresenter<V>
+        implements LifeCycleMvpPresenter<V> {
+    private LifecycleCompositeSubscription compositeSubscription = LifecycleCompositeSubscriptionFabric
+            .create();
 
+    protected final void addSubscription(Subscription subscription) {
+        compositeSubscription.addSubscription(subscription);
+    }
+
+    @Override
+    public void onCreate() {compositeSubscription.onCreate();}
+
+    @Override
+    public void onDestroy() {compositeSubscription.onDestroy();}
+
+    @Override
+    public void onStart() {compositeSubscription.onStart();}
+
+    @Override
+    public void onStop() {compositeSubscription.onStop();}
+
+    @Override
+    public void onResume() {compositeSubscription.onResume();}
+
+    @Override
+    public void onPause() {compositeSubscription.onPause();}
 }
--- app/src/main/java/com/nomensvyat/offlinechat/ui/BaseActivity.java
+++ app/src/main/java/com/nomensvyat/offlinechat/ui/BaseActivity.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 
 import com.hannesdorfmann.mosby.mvp.MvpActivity;
-import com.hannesdorfmann.mosby.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 import com.nomensvyat.offlinechat.OfflineChatApplication;
 import com.nomensvyat.offlinechat.di.activity.ActivityComponent;
 import com.nomensvyat.offlinechat.di.activity.ActivityModule;
 import com.nomensvyat.offlinechat.di.activity.DaggerActivityComponent;
 import com.nomensvyat.offlinechat.di.application.AppComponent;
+import com.nomensvyat.offlinechat.presentation.base.LifeCycleMvpPresenter;
 
-public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
+public abstract class BaseActivity<V extends MvpView, P extends LifeCycleMvpPresenter<V>>
         extends MvpActivity<V, P> {
 
     @CallSuper
@@ -20,6 +20,8 @@ public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
     protected void onCreate(Bundle savedInstanceState) {
         buildDiComponent();
         super.onCreate(savedInstanceState);
+
+        presenter.onCreate();
     }
 
     private void buildDiComponent() {
@@ -37,4 +39,39 @@ public abstract class BaseActivity<V extends MvpView, P extends MvpPresenter<V>>
      * Inject dependencies into activity with provided {@link ActivityComponent}
      */
     protected abstract void inject(ActivityComponent activityComponent);
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        presenter.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        presenter.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        presenter.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        presenter.onStop();
+    }
 }
