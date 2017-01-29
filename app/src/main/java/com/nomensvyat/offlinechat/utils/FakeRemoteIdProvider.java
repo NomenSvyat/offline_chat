c551089e596677e6f8bbdd0893c430e153213387
Chernov Svyatoslav <nomen.svyat@gmail.com>
2017-01-29 14:46:59 +0300

    Fix RemoteIdProvider

--- app/src/main/java/com/nomensvyat/offlinechat/utils/FakeRemoteIdProvider.java
+++ app/src/main/java/com/nomensvyat/offlinechat/utils/FakeRemoteIdProvider.java
@@ -1,21 +1,32 @@
 package com.nomensvyat.offlinechat.utils;
 
-import com.nomensvyat.offlinechat.di.application.PerApplication;
+import android.content.Context;
+import android.content.SharedPreferences;
 
-import java.util.concurrent.atomic.AtomicLong;
+import com.nomensvyat.offlinechat.di.application.PerApplication;
 
 import javax.inject.Inject;
 
 @PerApplication
 public class FakeRemoteIdProvider {
 
-    private AtomicLong id = new AtomicLong(0L);
+    private static final String FAKE_REMOTE = "fake_remote";
+    private static final String KEY_LAST_ID = "last_id";
+    private final SharedPreferences lastIdContainer;
 
     @Inject
-    public FakeRemoteIdProvider() {
+    public FakeRemoteIdProvider(Context context) {
+        lastIdContainer = context.getSharedPreferences(FAKE_REMOTE,
+                                                       Context.MODE_PRIVATE);
     }
 
     public long getAndIncrement() {
-        return id.getAndIncrement();
+        int lastId = lastIdContainer.getInt(KEY_LAST_ID, 0);
+
+        lastIdContainer.edit()
+                .putInt(KEY_LAST_ID, lastId++)
+                .apply();
+
+        return lastId;
     }
 }
